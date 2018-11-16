af27142c6557c0afdff044a6c0b3dfd31ac23c84
cpm3579 <40259035+haydencoinprima@users.noreply.github.com>
2018-11-16 09:37:16 +0800

    Update private_rest_api_client.js
    Modify the hmac authentication method.

--- private_rest_api_client.js
+++ private_rest_api_client.js
@@ -1,29 +1,13 @@
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-const uuid = require('uuid-random');
-const sha256 = require('js-sha256');
-const bytes = require('utf8-bytes')
+const HmacUtils = require('./hmacUtils');
 
-var METHOD = "POST";
-var URL = "YOUR_API_URL";
-var API_KEY = "YOUR_API_KEY";
-var SECRET_KEY = "YOUR_SECRET_KEY";
 var PREFIX = "CoinPrima";
+var API_KEY = "API_KEY";
+var SECRET_KEY = "SECRET_KEY";
+var METHOD = "GET"; //"POST", "DELETE"; 
+var URL = "URL";
 
-function GetHmacAuthHeader(method, endPoint, apikey, secret){
-    var currentTimeText = (new Date().getTime() / 1000).toFixed(0);
-    var uniqueID = uuid();
-    var uri = encodeURIComponent(endPoint).toLowerCase();
-
-    var data = apikey + method + uri + currentTimeText + uniqueID + "";
-    var encodedSecret = bytes(secret);
-    var encodedData = bytes(data);
-    var hash = sha256.hmac(encodedSecret, encodedData);
-    var parameter = apikey + ":" + hash + ":" + uniqueID + ":" + currentTimeText;
-    var authHeader = PREFIX + " " + parameter;
-    return authHeader;    
-}
-
-var auth = GetHmacAuthHeader(METHOD, URL, API_KEY, SECRET_KEY);
+var auth = HmacUtils.GetHmacAuthHeader(PREFIX, METHOD, URL, API_KEY, SECRET_KEY, "");
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
      if (this.readyState == 4 && this.status == 200) {
@@ -35,6 +19,6 @@ xhttp.onreadystatechange = function() {
 };
 
 xhttp.open(METHOD, URL, true);
-xhttp.setRequestHeader("Content-type", "application/json");
+xhttp.setRequestHeader("Content-Type", "application/json");
 xhttp.setRequestHeader("Authorization", auth);
 xhttp.send();
